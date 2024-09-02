8a5680c671ef42601efd601d9aae2bd40a8959c1
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 14:08:07 +0200

    FDsdfs'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,5 +18,6 @@ public class Main {
         System.out.println("Dobry");
         System.out.println("Dobry");
         System.out.println("Dobry");
+        System.out.println("Dobry");
     }
 }
