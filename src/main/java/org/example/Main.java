9289163cdfac1f09fa5dc32406d183eb3b86a086
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 21:40:42 +0200

    ziobry 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Siema HENIU!");
         System.out.println("Ziobry");
+        System.out.println("Ziobry");
     }
 }
