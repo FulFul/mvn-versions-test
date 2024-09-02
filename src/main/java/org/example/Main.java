6c97247ebab2e6a822d890dafc75d66d05fa5a3a
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 11:14:58 +0200

    hotfix x5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Siema HENIU!");
         System.out.println("Siema HENIU!");
-        System.out.println("Siema HENIU! hotfix w drodze");
+        System.out.println("Siema HENIU! hotfix w drodze - x5");
     }
 }
