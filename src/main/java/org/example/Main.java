adf7384b362d075f5867e6ecc45dccacf5d27784
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 16:58:21 +0200

    hotfix 0000

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,5 +30,6 @@ public class Main {
         System.out.println("Dobry");
         System.out.println("Dobry");
         System.out.println("Dobry");
+        System.out.println("Dobry");
     }
 }
