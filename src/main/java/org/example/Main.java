459026ecbda945875963fea3b0ab8d2a61673b2a
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 14:18:12 +0200

    Fsdfsd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,5 +19,6 @@ public class Main {
         System.out.println("Dobry");
         System.out.println("Dobry");
         System.out.println("Dobry");
+        System.out.println("Dobry");
     }
 }
