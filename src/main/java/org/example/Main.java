98968e263b7438e389068d55b64151ade1e1a1d0
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 21:45:46 +0200

    ziobry3 commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Ziobry");
         System.out.println("Ziobry");
         System.out.println("Ziobry");
+        System.out.println("Ziobry");
     }
 }
