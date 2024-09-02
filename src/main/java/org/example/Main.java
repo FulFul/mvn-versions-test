b4607792498b1ad06bbca783302db0ab49bd5357
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 12:35:29 +0200

    dobry

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Siema HENIU!");
-        System.out.println("Siema HENIU!");
-        System.out.println("Siema HENIU! hotfix w drodze");
-        System.out.println("Siema HENIU! hotfix w drodze");
-        System.out.println("Siema HENIU! hotfix w drodze");
-        System.out.println("Siema HENIU! hotfix w drodze - x555");
-        System.out.println("Siema HENIU! hotfix w drodze - x555");
+        System.out.println("Dobry");
     }
 }
