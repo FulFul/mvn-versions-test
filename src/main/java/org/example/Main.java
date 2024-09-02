6c4e0e443c15bcf1a3a30a9a4f62ff377196c523
Grzegorz Fula <grzegorz.fula@gft.com>
2024-09-02 14:28:13 +0200

    commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,5 +21,6 @@ public class Main {
         System.out.println("Dobry");
         System.out.println("Dobry");
         System.out.println("Dobry");
+        System.out.println("Dobry");
     }
 }
